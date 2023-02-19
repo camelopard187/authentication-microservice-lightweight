1949661c5058d963aab30c8e1e6a2e6fb56ffa90
camelopard187 <camelopard187@gmail.com>
2023-02-19 19:22:40 +0200

    feat: Add a request body validation handler ✨

--- source/domain/authentication/credential/model.ts
+++ source/domain/authentication/credential/model.ts
@@ -1,5 +1,13 @@
-export type Name = string
-export type Email = string
-export type Password = string
+import { object, string, infer as inferred } from 'zod'
 
-export type Credential = { name: Name; email: Email; password: Password }
+export const name = string()
+
+export const email = string().email('Provided the wrong email')
+
+export const password = string()
+  .min(8, 'Password is too short - should be 8 chars minimum')
+  .max(24, 'Password is too long - should be 24 chars maximum')
+
+export const credential = object({ name, email, password }).required()
+
+export type Credential = inferred<typeof credential>
--- source/periphery/presentation/authentication/controller.ts
+++ source/periphery/presentation/authentication/controller.ts
@@ -4,6 +4,11 @@ import type { RouterOptions } from 'express'
 import { registerHandler } from './credential/register-handler'
 import { loginHandler } from './credential/login-handler'
 import { refreshHandler } from './token/refresh-handler'
+import { validate } from '../common/validate-handler'
+import { credential } from '../../../domain/authentication/credential/model'
 
 export const controller = (options?: RouterOptions) =>
-  Router(options).post('/register', registerHandler).post('/login', loginHandler).post('/refresh', refreshHandler)
+  Router(options)
+    .post('/register', validate(credential), registerHandler)
+    .post('/login', validate(credential.pick({ email: true, password: true })), loginHandler)
+    .post('/refresh', refreshHandler)

7f5172ca1f5ae11f1db21eac00f3c14131c1489e
camelopard187 <camelopard187@gmail.com>
2023-05-31 16:24:26 +0300

    refactor: Simplify the register function 📦

--- source/application/authentication/credential/register.ts
+++ source/application/authentication/credential/register.ts
@@ -1,13 +1,13 @@
 import { hash } from 'bcryptjs'
-import { assoc } from 'rambda'
 
 import { authenticate } from '../../common/authentication/authenticate'
 import { insertCredential } from '../../../periphery/persistence/repository/credential'
 import type { AuthenticationDetails } from '../../common/authentication/authenticate'
 import type { Credential } from '../../../domain/authentication/credential/model'
 
-const hashCredential = async (credential: Credential) =>
-  assoc('password', await hash(credential.password, 10), credential)
-
-export const register = (credential: Credential): Promise<AuthenticationDetails> =>
-  hashCredential(credential).then(insertCredential).then(authenticate)
+export const register = (
+  credential: Credential
+): Promise<AuthenticationDetails> =>
+  hash(credential.password, 10)
+    .then(password => insertCredential({ ...credential, password }))
+    .then(authenticate)
